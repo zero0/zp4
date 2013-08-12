943afbbe499f2263631830c92dd28ba45ea05d82
justin <phosgene84@gmail.com>
2013-08-12 08:09:59 +0000

    - Added materials folder and fullscreenNoAlpha material - Updated fullscreenNoAlpha.shader to use the new JSON shader format

--- ZeroPoint4/Blackjack/Bin/Assets/shaders/fullscreenNoAlpha.hlsl
+++ ZeroPoint4/Blackjack/Bin/Assets/shaders/fullscreenNoAlpha.hlsl
@@ -2,24 +2,28 @@
 Texture2D<float4> colorMap : register( t0 );
 SamplerState pointSampler : register( s0 );
 
-struct VS_Input {
-	float4 position : POSITION;
-	float2 tex0 : TEXCOORD;
+struct VS_Input
+{
+    float4 position : POSITION;
+    float2 tex0 : TEXCOORD;
 };
 
-struct PS_Input {
-	float4 position : SV_POSITION;
-	float2 tex0 : TEXCOORD;
+struct PS_Input
+{
+    float4 position : SV_POSITION;
+    float2 tex0 : TEXCOORD;
 };
 
-PS_Input main_vs( VS_Input input ) {
-	PS_Input output = (PS_Input)0;
-	output.position = input.position;
-	output.tex0 = input.tex0;
-	
-	return output;
+PS_Input main_vs( VS_Input input )
+{
+    PS_Input output = (PS_Input)0;
+    output.position = input.position;
+    output.tex0 = input.tex0;
+    
+    return output;
 }
 
-float4 main_ps( PS_Input input ) : SV_TARGET {
-	return float4( colorMap.Sample( pointSampler, input.tex0 ).rgb, 1.f );
+float4 main_ps( PS_Input input ) : SV_TARGET
+{
+    return float4( colorMap.Sample( pointSampler, input.tex0 ).rgb, 1.f );
 }
