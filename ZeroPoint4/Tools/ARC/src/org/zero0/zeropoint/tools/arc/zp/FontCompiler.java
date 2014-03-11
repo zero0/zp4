c2054f33738f8be6f6931b0c661fb556689fe209
justin <phosgene84@gmail.com>
2014-03-11 07:26:43 +0000

    - Added FontCompiler to ARC build process - Added test font

---
+++ ZeroPoint4/Tools/ARC/src/org/zero0/zeropoint/tools/arc/zp/FontCompiler.java
@@ -0,0 +1,15 @@
+package org.zero0.zeropoint.tools.arc.zp;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.zero0.zeropoint.tools.arc.compiler.ArcCompiler;
+
+public class FontCompiler extends ArcCompiler
+{
+	@Override
+	protected List<String> getCompilerParams()
+	{
+		return Arrays.asList( getFileToCompile(), getOutputFile() );
+	}
+}
