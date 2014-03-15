27747c0ead8cad987a0608cf67d9133a2557889b
justin <phosgene84@gmail.com>
2014-03-15 02:35:22 +0000

    - Updated FontCompiler to parse XML from a .fnt file

--- ZeroPoint4/FontCompiler/FontCompiler.cpp
+++ ZeroPoint4/FontCompiler/FontCompiler.cpp
@@ -1,159 +1,129 @@
 #include "FontCompiler.h"
 
-void compileBitmapFont( const zpString& inputFile, const zpString& outputFile )
+zp_bool compileBitmapFontXML( const zpString& inputFile, zpJson& outputJson )
 {
-	zpString lineStr, name;
-	zpStringBuffer line;
-	zpArrayList< zpString > parts, subParts, arrayParts;
-	zpJson outputJson;
-
-	zpFile file( inputFile );
-	if( file.open( ZP_FILE_MODE_ASCII_READ ) )
+	zp_bool ok = false;
+	zpXmlParser xmlParser;
+	if( xmlParser.parseFile( inputFile, true ) )
 	{
-		while( !file.isEOF() )
+		ok = true;
+		if( xmlParser.push( zpString( "font" ) ) )
 		{
-			file.readLine( line );
-			if( line.isEmpty() ) continue;
-
-			lineStr = line.str();
-			lineStr.split( ' ', parts );
-			if( parts.isEmpty() ) continue;
-
-			const zp_char* type = parts[0].str();
-
-			if( zp_strcmp( type, "info" ) == 0 )
+			if( xmlParser.push( zpString( "info" ) ) )
 			{
-				for( zp_int i = 1; parts.size(); ++i )
-				{
-					parts[i].split( '=', subParts );
+				zpXmlNode* info = xmlParser.getCurrentNode();
+				outputJson[ "face" ] =		zpJson( info->attributes.getString( "face" ) );
+				outputJson[ "size" ] =		zpJson( info->attributes.getInt( "size" ) );
+				outputJson[ "bold" ] =		zpJson( info->attributes.getInt( "bold" ) );
+				outputJson[ "italic" ] =	zpJson( info->attributes.getInt( "italic" ) );
+				outputJson[ "charset" ] =	zpJson( info->attributes.getString( "charset" ) );
+				outputJson[ "unicode" ] =	zpJson( info->attributes.getInt( "unicode" ) );
+				outputJson[ "stretchH" ] =	zpJson( info->attributes.getInt( "stretchH" ) );
+				outputJson[ "smooth" ] =	zpJson( info->attributes.getInt( "smooth" ) );
+				outputJson[ "aa" ] =		zpJson( info->attributes.getInt( "aa" ) );
+				outputJson[ "outline" ] =	zpJson( info->attributes.getInt( "outline" ) );
 
-					subParts[0].toLower( name );
-					const zpString& value = subParts[1];
+				//outputJson[ "padding" ] = zpJson( info->attributes.getString( "padding" ) );
+				//outputJson[ "spacing" ] = zpJson( info->attributes.getString( "spacing" ) );
 
-					// string
-					if( value.startsWith( '"' ) && value.endsWith( '"' ) )
-					{
-						zpString v;
-						value.substring( v, 1, -1 );
-						outputJson[ name ] = zpJson( v );
-					}
-					// array
-					else if( value.indexOf( ',' ) != -1 )
-					{
-						value.split( ',', arrayParts );
-						zpJson arr;
-						arrayParts.foreachIndexed( [ &arr ]( zp_uint index, const zpString& value ) {
-							zp_int v = 0;
-							v = zp_atoi( value.str() );
-							arr[ index ] = zpJson( v );
-						} );
-						outputJson[ name ] = arr;
-					}
-					// number
-					else
-					{
-						zp_int v = 0;
-						v = zp_atoi( value.str() );
-						outputJson[ name ] = zpJson( v );
-					}
-				}
+				xmlParser.pop();
 			}
-			else if( zp_strcmp( type, "common" ) == 0 )
+
+			if( xmlParser.push( zpString( "common" ) ) )
 			{
-				for( zp_int i = 1; parts.size(); ++i )
-				{
-					parts[i].split( '=', subParts );
+				zpXmlNode* common = xmlParser.getCurrentNode();
+				outputJson[ "lineHeight" ] =	zpJson( common->attributes.getInt( "lineHeight" ) );
+				outputJson[ "base" ] =			zpJson( common->attributes.getInt( "base" ) );
+				outputJson[ "scaleW" ] =		zpJson( common->attributes.getInt( "scaleW" ) );
+				outputJson[ "scaleH" ] =		zpJson( common->attributes.getInt( "scaleH" ) );
+				//outputJson[ "pages" ] =			zpJson( common->attributes.getInt( "pages" ) );
+				outputJson[ "packed" ] =		zpJson( common->attributes.getInt( "packed" ) );
+				outputJson[ "alphaChnl" ] =		zpJson( common->attributes.getInt( "alphaChnl" ) );
+				outputJson[ "redChnl" ] =		zpJson( common->attributes.getInt( "redChnl" ) );
+				outputJson[ "greenChnl" ] =		zpJson( common->attributes.getInt( "greenChnl" ) );
+				outputJson[ "blueChnl" ] =		zpJson( common->attributes.getInt( "blueChnl" ) );
 
-					subParts[0].toLower( name );
-					const zpString& value = subParts[1];
-
-					zp_int v = 0;
-					v = zp_atoi( value.str() );
-					outputJson[ name ] = zpJson( v );
-				}
+				xmlParser.pop();
 			}
-			else if( zp_strcmp( type, "page" ) == 0 )
+
+			if( xmlParser.push( zpString( "pages" ) ) )
 			{
-				zpJson& pages = outputJson[ "pages" ];
-
-				zp_int pageId = 0;
-				for( zp_int i = 1; parts.size(); ++i )
+				if( xmlParser.push( zpString( "page" ) ) )
 				{
-					parts[i].split( '=', subParts );
-
-					const zpString& id = subParts[0];
-					const zpString& value = subParts[1];
-
-					if( zp_strcmp( id.str(), "id" ) == 0 )
+					zpJson& pages = outputJson[ "pages" ];
+					do
 					{
-						pageId = zp_atoi( value.str() );
+						zpXmlNode* page = xmlParser.getCurrentNode();
+						zp_int id = page->attributes.getInt( "id" );
+						const zpString& file = page->attributes.getString( "file" );
+
+						pages[ id ] = zpJson( file );
 					}
-					else if( zp_strcmp( id.str(), "page" ) == 0 )
+					while( xmlParser.next() );
+
+					xmlParser.pop();
+				}
+				xmlParser.pop();
+			}
+
+			if( xmlParser.push( zpString( "chars" ) ) )
+			{
+				zpXmlNode* cs = xmlParser.getCurrentNode();
+				zp_int count = cs->attributes.getInt( "count" );
+
+				if( xmlParser.push( zpString( "char" ) ) )
+				{
+					zpJson& chars = outputJson[ "chars" ];
+					do
 					{
-						zpString v;
-						value.substring( v, 1, -1 );
-
-						pages[ pageId ] = zpJson( v );
+						zpXmlNode* c = xmlParser.getCurrentNode();
+						zpJson& ch = chars[ chars.size() ];
+						ch[ "id" ] =		zpJson( c->attributes.getInt( "id" ) );
+						ch[ "x" ] =			zpJson( c->attributes.getInt( "x" ) );
+						ch[ "y" ] =			zpJson( c->attributes.getInt( "y" ) );
+						ch[ "width" ] =		zpJson( c->attributes.getInt( "width" ) );
+						ch[ "height" ] =	zpJson( c->attributes.getInt( "height" ) );
+						ch[ "xoffset" ] =	zpJson( c->attributes.getInt( "xoffset" ) );
+						ch[ "yoffset" ] =	zpJson( c->attributes.getInt( "yoffset" ) );
+						ch[ "xadvance" ] =	zpJson( c->attributes.getInt( "xadvance" ) );
+						ch[ "page" ] =		zpJson( c->attributes.getInt( "page" ) );
+						ch[ "chnl" ] =		zpJson( c->attributes.getInt( "chnl" ) );
 					}
+					while( xmlParser.next() );
+
+					xmlParser.pop();
 				}
+				xmlParser.pop();
 			}
-			//else if( zp_strcmp( type, "chars" ) == 0 )
-			//{
-			//	parts[1].split( '=', subParts );
-			//
-			//	const zpString& value = subParts[1];
-			//
-			//	zp_int v = 0;
-			//	v = zp_atoi( value.str() );
-			//	json[ name ] = zpJson( v );
-			//}
-			else if( zp_strcmp( type, "char" ) == 0 )
+
+			if( xmlParser.push( zpString( "kernings" ) ) )
 			{
-				zpJson& chars = outputJson[ "chars" ];
-				zpJson& c = chars[ chars.size() ];
-				for( zp_int i = 1; parts.size(); ++i )
+				zpXmlNode* kernings = xmlParser.getCurrentNode();
+				zp_int count = kernings->attributes.getInt( "count" );
+
+				if( xmlParser.push( zpString( "kerning" ) ) )
 				{
-					parts[i].split( '=', subParts );
+					zpJson& kernings = outputJson[ "kernings" ];
+					do
+					{
+						zpXmlNode* k = xmlParser.getCurrentNode();
+						zpJson& kerning = kernings[ kernings.size() ];
+						kerning[ "first" ] =	zpJson( k->attributes.getInt( "first" ) );
+						kerning[ "second" ] =	zpJson( k->attributes.getInt( "second" ) );
+						kerning[ "amount" ] =	zpJson( k->attributes.getInt( "amount" ) );
+					}
+					while( xmlParser.next() );
 
-					subParts[0].toLower( name );
-					const zpString& value = subParts[1];
-
-					zp_int v = 0;
-					v = zp_atoi( value.str() );
-					c[ name ] = zpJson( v );
+					xmlParser.pop();
 				}
+				xmlParser.pop();
 			}
-			//else if( zp_strcmp( type, "kernings" ) == 0 )
-			//{
-			//
-			//}
-			else if( zp_strcmp( type, "kerning" ) == 0 )
-			{
-				zpJson& kernings = outputJson[ "kernings" ];
-				zpJson& k = kernings[ kernings.size() ];
-				for( zp_int i = 1; parts.size(); ++i )
-				{
-					parts[i].split( '=', subParts );
 
-					subParts[0].toLower( name );
-					const zpString& value = subParts[1];
-
-					zp_int v = 0;
-					v = zp_atoi( value.str() );
-					k[ name ] = zpJson( v );
-				}
-			}
-		}
-
-		if( zpBison::compileToFile( outputFile, outputJson ) )
-		{
-			zpLog::message() << "Successfully compiled '" << outputFile << "'" << zpLog::endl;
-		}
-		else
-		{
-			zpLog::error() << "Unable to compile output file '" << outputFile << "'" << zpLog::endl;
+			xmlParser.pop();
 		}
 	}
+
+	return ok;
 }
 
 void compileTrueTypeFont( const zpString& inputFile, const zpString& outputFile )
@@ -184,16 +154,36 @@ zp_int main( zp_int argCount, const zp_char* args[] )
 	}
 	else
 	{
+		zp_bool ok = false;
+		zpJson outputJson;
 		const zpString& inputFile = arguments[ 0 ];
 		const zpString& outputFile = arguments[ 1 ];
 
 		if( inputFile.endsWith( ".fnt" ) )
 		{
-			compileBitmapFont( inputFile, outputFile );
+			ok = compileBitmapFontXML( inputFile, outputJson );
 		}
 		else if( inputFile.endsWith( ".ttf" ) )
 		{
-			compileTrueTypeFont( inputFile, outputFile );
+			//compileTrueTypeFont( inputFile, outputFile );
+		}
+
+		// if compilation ok
+		if( ok )
+		{
+			if( zpBison::compileToFile( outputFile, outputJson ) )
+			{
+				zpLog::message() << "Successfully compiled '" << outputFile << "'" << zpLog::endl;
+			}
+			else
+			{
+				zpLog::error() << "Unable to compile output file '" << outputFile << "'" << zpLog::endl;
+			}
+		}
+		// failed to compile
+		else
+		{
+			zpLog::error() << "Failed to compile '" << outputFile << "'" << zpLog::endl;
 		}
 	}
 
