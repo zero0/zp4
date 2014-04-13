546634e74cb7188b803710f8b2ad2d8d5bbf5165
justin <phosgene84@gmail.com>
2014-04-13 06:51:42 +0000

    - Added font material and updated arial32 to use XML - Added TransparentDiffuse shader

---
+++ ZeroPoint4/Blackjack/Bin/Assets/shaders/TransparentDiffuse.hlsl
@@ -0,0 +1,64 @@
+#pragma pack_matrix( row_major )
+
+SamplerState diffuseSampler : register( s0 );
+Texture2D<float4> diffuse : register( t0 );
+
+cbuffer Camera : register( b0 )
+{
+	float4x4 viewProjection;
+	float4x4 invViewProjection;
+
+	float4 up;
+	float4 lookTo;
+	float4 position;
+    
+	float zNear;
+	float zFar;
+	float fovy;
+	float aspectRatio;
+};
+
+cbuffer PerFrame : register( b1 )
+{
+	float deltaTime;
+	float actualDeltaTime;
+	float fixedDeltaTime;
+	float timeFromStart;
+};
+
+cbuffer PerDrawCall : register( b2 )
+{
+	float4x4 world;
+};
+
+struct VS_Input
+{
+	float4 position : POSITION;
+	float4 color : COLOR;
+	float2 uv0 : TEXCOORD0;
+};
+
+struct PS_Input
+{
+	float4 position : SV_POSITION;
+	float4 color : COLOR;
+	float2 uv0 : TEXCOORD0;
+};
+
+PS_Input main_vs( VS_Input input )
+{
+	PS_Input output = (PS_Input)0;
+	output.position = input.position;
+	output.position = mul( output.position, world );
+	output.position = mul( output.position, viewProjection );
+	output.color = input.color;
+	output.uv0 = input.uv0;
+
+	return output;
+}
+
+float4 main_ps( PS_Input input ) : SV_TARGET
+{
+	float4 col = diffuse.Sample( diffuseSampler, input.uv0 );
+	return col * input.color;
+}
---
+++ ZeroPoint4/Blackjack/Bin/Assets/shaders/TransparentDiffuse.shader
@@ -0,0 +1,12 @@
+{
+    "File" : "TransparentDiffuse.hlsl",
+    "VS" : 
+    {
+        "Main" : "main_vs",
+        "Format" : "VCU",
+    },
+    "PS" :
+    {
+        "Main" : "main_ps"
+    },
+}
