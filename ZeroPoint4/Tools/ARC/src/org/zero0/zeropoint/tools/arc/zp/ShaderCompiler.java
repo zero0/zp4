2ef71e6ad6f604a744125a804c8fddaa98ac598a
justin <phosgene84@gmail.com>
2014-04-15 04:12:17 +0000

    - Updated ShaderCompiler to select different optimization level based on debug or release modes

--- ZeroPoint4/Tools/ARC/src/org/zero0/zeropoint/tools/arc/zp/ShaderCompiler.java
+++ ZeroPoint4/Tools/ARC/src/org/zero0/zeropoint/tools/arc/zp/ShaderCompiler.java
@@ -3,12 +3,21 @@ package org.zero0.zeropoint.tools.arc.zp;
 import java.util.Arrays;
 import java.util.List;
 
+import org.zero0.zeropoint.tools.arc.Arc;
 import org.zero0.zeropoint.tools.arc.compiler.ArcCompiler;
 
 public class ShaderCompiler extends ArcCompiler
 {
 	protected List<String> getCompilerParams()
 	{
-		return Arrays.asList( getRendering().name(), "-Od", getFileToCompile(), getOutputFile() );
+		char option = 'd';
+		switch( Arc.getInstance().getExecutableMode() )
+		{
+		case Release:
+			option = '1';
+			break;
+		}
+
+		return Arrays.asList( getRendering().name(), "-O" + option, getFileToCompile(), getOutputFile() );
 	}
 }
